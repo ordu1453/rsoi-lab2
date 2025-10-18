2d9d0450e654ab2f8f9f72df44f8e5120ded7e7c
ordu1453 <1muzaffer.4ali.5ordu3@gmail.com>
2025-10-18 22:14:40 +0300

    small fixes

--- v4/gateway/app.py
+++ v4/gateway/app.py
@@ -11,8 +11,49 @@ RESERVATION_URL = "http://reservation_service:8070"
 # Пример: получить список всех библиотек
 @app.route("/api/v1/libraries", methods=["GET"])
 def get_libraries():
-    resp = requests.get(f"{LIBRARY_URL}/libraries")
-    return jsonify(resp.json()), resp.status_code
+    city = request.args.get('city')
+    category = request.args.get("category")
+    page = request.args.get("page")
+
+    # Формируем параметры для другого сервиса
+    params = {
+        "category": category,
+        "page": page,
+        "city": city
+    }
+
+    response = requests.get(f"{LIBRARY_URL}/libraries", params=params, timeout=5)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print(data)
+        except ValueError:
+            print("Ответ не в формате JSON:", response.text)
+    else:
+        print(f"Ошибка {response.status_code}: {response.text}")
+    return jsonify(response.json()), response.status_code
+
+@app.route('/api/v1/libraries/<library_uid>/books', methods=['GET'])
+def get_books(library_uid):
+    category = request.args.get("category")
+    page = request.args.get("page")
+
+    params = {
+        "category": category,
+        "page": page
+    }
+
+    response = requests.get(f"{LIBRARY_URL}/libraries/{library_uid}/books", params=params, timeout=5)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print(data)
+        except ValueError:
+            print("Ответ не в формате JSON:", response.text)
+    else:
+        print(f"Ошибка {response.status_code}: {response.text}")
+    return jsonify(response.json()), response.status_code
+
 
 # Пример: получить рейтинг книги
 @app.route("/api/v1/books/<book_uid>/rating", methods=["GET"])
@@ -32,6 +73,14 @@ def create_reservation():
 def get_reservations():
     user_id = request.args.get("user_id")
     resp = requests.get(f"{RESERVATION_URL}/reservations?user_id={user_id}")
+    return jsonify(resp.json()), resp.status_code
+
+
+@app.route("/api/v1/rating", methods = ["GET"])
+def get_rating():
+    user_name = request.args.get("X-User-Name")
+    params = {"X-User_Name" : user_name}
+    resp = requests.get(f"{RATING_URL}/ratings", params=params, timeout=5)
     return jsonify(resp.json()), resp.status_code
 
 # Health check
